eaaa088ea4b958739015b203cf0b4381de8fc4ab
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-22 19:30:35 +0100

    reset updat password signout function as class method

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -37,6 +37,12 @@ class Firebase {
 
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
