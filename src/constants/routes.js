1b051e8a96825298555265d20b96a19f7858be9a
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-25 01:48:02 +0100

    npm i react-async-script-loader

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,4 +4,5 @@ export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
 export const ADMIN = '/admin';
+export const MAP = '/map';
 export const PASSWORD_FORGET = '/pw-forget';
