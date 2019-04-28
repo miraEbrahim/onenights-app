476a69a7033234748ef607be5285156e759a67c8
Maryam Ebrahim <maryam@miras-mbp.home>
2019-04-28 17:27:10 +0100

    added axios and foursquare

--- src/components/Map/MapData.js
+++ src/components/Map/MapData.js
@@ -1,3 +1,7 @@
 export const G_API = 'AIzaSyAvvgbXQPqd-UKFbNBjvpxUsAXtX-1LXow';
 
 export const london = { lat: 51.5074, lng: 0.1278 };
+
+//Foursquare data
+export const FS_ID = 'PO0J5LXJMTUIHJCTJ21KRY3LZ14FP5DBJBFIWJ40XZCDWP0A';
+export const FS_SEC = 'LV4LZPP5EJJHY5SQKMWRMHFCJSDS0TRPAXXENRSAEEXRRTTG';
--- src/components/Map/MapPlaces.js
+++ src/components/Map/MapPlaces.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
-import { london, G_API } from './MapData';
+
+import axios from 'axios';
+
+import { london, G_API, FS_ID, FS_SEC } from './MapData';
 import { mapDarkStyle } from './MapStyle';
 import './mapstyle.css';
 
 class MapPlaces extends Component {
   componentDidMount() {
-    this.renderMap();
+    this.getVenues();
+    this.loadMap();
   }
 
-  renderMap = () => {
+  loadMap = () => {
     loadScript(
       `https://maps.googleapis.com/maps/api/js?key=${G_API}&callback=initMap`
     );
     window.initMap = this.initMap;
   };
 
+  //Foursquer API
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = {
+      client_id: FS_ID,
+      client_secret: FS_SEC,
+      near: 'london',
+      v: '20190428'
+    };
+
+    //axios get request
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        console.log(response.data.response.groups[0].items);
+      })
+      .catch(error => {
+        console.log("Error get Foursquare API's Data" + error);
+      });
+  };
+
   initMap = () => {
     // eslint-disable-next-line
-    var map = new window.google.maps.Map(document.getElementById('map'), {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
       center: london,
       zoom: 10,
       style: mapDarkStyle
